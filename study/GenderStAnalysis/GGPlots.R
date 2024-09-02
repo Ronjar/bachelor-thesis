4d9405f54d86a9a379733ce66ac0d1663716cec0
Ronjar <robingebert99@gmail.com>
2024-09-02 16:21:27 +0200

    Implemented Marias suggestions part 1

--- study/GenderStAnalysis/GGPlots.R
+++ study/GenderStAnalysis/GGPlots.R
@@ -8,64 +8,79 @@ calculate_stats <- function(data, group_variables, value_variable) {
   data %>%
     group_by(across(all_of(group_variables))) %>%
     summarise(
-      Mean = mean({{ value_variable }}, na.rm = TRUE),
+      Mean = mean({{ value_variable }}, na.rm = TRUE),  # Berechnet den absoluten Wert des Mittelwerts
       SE = sd({{ value_variable }}, na.rm = TRUE) / sqrt(n()),
       .groups = 'drop'
     )
 }
 
-# Funktion zum Erstellen von Balkendiagrammen
+# Funktion zur Anpassung der Legendenbeschriftung
+capitalize_labels <- function(labels) {
+  sapply(labels, function(label) {
+    paste(toupper(substring(label, 1, 1)), substring(label, 2), sep = "")
+  })
+}
+
+# Funktion zum Erstellen von Balkendiagrammen mit Unterstützung für negative Werte und Achsenspiegelung
 plot_data <- function(data, xlab, ylab, by_gender = FALSE) {
+  
   if (by_gender) {
-    ggplot(data, aes(x = gamifiedElement, y = Mean, fill = gender)) +
+    p <- ggplot(data, aes(x = gamifiedElement, y = Mean, fill = gender)) +
       geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.7) +
       geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE),
                     width = 0.25, position = position_dodge(width = 0.8)) +
       labs(x = xlab, y = ylab) +
-      ylim(0, 1) +
-      theme_minimal() +
+      theme_minimal(base_family = "Arial") +
+      guides(fill = guide_legend(title = "Gender", label.position = "right",
+                                 labels = capitalize_labels)) +
       theme(
-        panel.grid.major = element_blank(), 
+        panel.background = element_blank(),
+        panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        text = element_text(size = 18),  # Textgröße erhöhen
-        axis.text.x = element_text(angle = -45, hjust = 0, vjust = 0, size = 16),  # X-Achsenbeschriftung drehen
-        axis.text.y = element_text(size = 16),  # Y-Achsenbeschriftung Textgröße
-        axis.title.x = element_text(size = 18, margin = margin(t = 20)),  # X-Achsentitel Textgröße und Abstand
-        axis.title.y = element_text(size = 18, margin = margin(r = 20)),  # Y-Achsentitel Textgröße und Abstand
-        legend.title = element_text(size = 16),  # Legendentitel Textgröße
-        legend.text = element_text(size = 16),  # Legendeneinträge Textgröße
+        text = element_text(size = 25),  # Textgröße erhöhen
+        axis.text.x = element_text(angle = -45, hjust = 0, vjust = 0, size = 22),  # X-Achsenbeschriftung drehen
+        axis.text.y = element_text(size = 25),  # Y-Achsenbeschriftung Textgröße
+        axis.title.x = element_text(size = 25, margin = margin(t = 20)),  # X-Achsentitel Textgröße und Abstand
+        axis.title.y = element_text(size = 25, margin = margin(r = 20)),  # Y-Achsentitel Textgröße und Abstand
+        legend.title = element_text(size = 25),  # Legendentitel Textgröße
+        legend.text = element_text(size = 25),  # Legendeneinträge Textgröße
         plot.margin = margin(t = 20, r = 20, b = 20, l = 20)  # Ränder hinzufügen
-      )
+      ) +
+      scale_y_continuous(labels = scales::comma, limits = c(floor(min(data$Mean - data$SE)), ceiling(max(data$Mean + data$SE))))
   } else {
-    ggplot(data, aes(x = gamifiedElement, y = Mean, fill = "color")) +
+    p <- ggplot(data, aes(x = gamifiedElement, y = Mean, fill = "color")) +
       geom_bar(stat = "identity", position = "dodge", width = 0.7) +
       geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE),
                     width = 0.25, position = position_dodge(width = 0.7)) +
       scale_fill_manual(values = c("color" = "#f8bc6d")) +
       labs(x = xlab, y = ylab) +
-      ylim(0, 1) +
-      theme_minimal() +
+      theme_minimal(base_family = "Arial") +
       theme(
-        panel.grid.major = element_blank(), 
+        panel.background = element_blank(),
+        panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        text = element_text(size = 18),  # Textgröße erhöhen
-        axis.text.x = element_text(angle = -45, hjust = 0, vjust = 0, size = 16),  # X-Achsenbeschriftung drehen
-        axis.text.y = element_text(size = 16),  # Y-Achsenbeschriftung Textgröße
-        axis.title.x = element_text(size = 18, margin = margin(t = 20)),  # X-Achsentitel Textgröße und Abstand
-        axis.title.y = element_text(size = 18, margin = margin(r = 20)),  # Y-Achsentitel Textgröße und Abstand
+        text = element_text(size = 25),  # Textgröße erhöhen
+        axis.text.x = element_text(angle = -45, hjust = 0, vjust = 0, size = 22),  # X-Achsenbeschriftung drehen
+        axis.text.y = element_text(size = 25),  # Y-Achsenbeschriftung Textgröße
+        axis.title.x = element_text(size = 25, margin = margin(t = 20)),  # X-Achsentitel Textgröße und Abstand
+        axis.title.y = element_text(size = 25, margin = margin(r = 20)),  # Y-Achsentitel Textgröße und Abstand
         legend.position = "none",  # Legende entfernen
-        plot.margin = margin(t = 20, r = 100, b = 20, l = 20)  # Ränder hinzufügen
-      )
+        plot.margin = margin(t = 20, r = 140, b = 20, l = 20)  # Ränder hinzufügen
+      ) +
+      scale_y_continuous(labels = scales::comma, limits = c(floor(min(data$Mean - data$SE)), ceiling(max(data$Mean + data$SE))))
+  }    
+  # Achsen spiegeln, wenn negative Werte vorhanden sind
+  if (any(data$Mean < 0)) {
+    p <- p + scale_y_reverse()
   }
+  return(p)
 }
 
 
 
 
-
-
 # Globale Variable für den Speicherpfad
-output_folder <<- "C:\\Users\\robin\\Documents\\GitHub\\bachelor-thesis\\img\\plots\\2"
+output_folder <<- "C:\\Users\\robin\\Documents\\GitHub\\bachelor-thesis\\img\\plots\\4"
 
 # Funktion zum Speichern von Plots mit Ordnerauswahl
 save_ggplot <- function(plot, plot_name) {
@@ -73,7 +88,7 @@ save_ggplot <- function(plot, plot_name) {
     dir.create(output_folder)
   }
   file_path <- file.path(output_folder, paste0(plot_name, ".png"))
-  ggsave(file_path, plot, height=20, width =30, units ="cm", dpi=800, bg="white")
+  ggsave(file_path, plot, height=20, width =30, units ="cm", dpi=800, bg="transparent")
 }
 
 # Hauptdatensatz
@@ -87,9 +102,9 @@ agg_data_anxiety <- calculate_stats(dat_gender, "gamifiedElement", stai)
 agg_data_performance_gender_no_ge <- calculate_stats(dat_gender, "gender", percentage_correct)
 
 # Daten visualisieren
-plot_performance_gender <- plot_data(agg_data_performance_gender, "Gamified Element", "Average percentage of correct answers", TRUE)
+plot_performance_gender <- plot_data(agg_data_performance_gender, "Gamified Element", "Average correct answers", TRUE)
 plot_anxiety_gender <- plot_data(agg_data_anxiety_gender, "Gamified Element", "Average anxiety level", TRUE)
-plot_performance <- plot_data(agg_data_performance, "Gamified Element", "Average percentage of correct answers")
+plot_performance <- plot_data(agg_data_performance, "Gamified Element", "Average correct answers")
 plot_anxiety <- plot_data(agg_data_anxiety, "Gamified Element", "Average anxiety level")
 
 # Diagramme anzeigen
